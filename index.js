7995f9a9a58ecfbca7eef084b7af315e8ed83126
Tibu EngineF <tibu@enginef.com>
2021-12-09 00:23:59 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import { CountryPicker as CountryPickerComponent } from "./src/screens";
+import React from "react";
+
+const DEFAULT_OPTIONS = {
+  onSelectCountry: () => {},
+  style: {},
+  showFlag: true,
+  showCallingCode: true,
+  showCountryName: true,
+  showNationalityName: true,
+  darkMode: true,
+};
+
+export default CountryPicker = (props) => {
+  const propsModel = {
+    ...DEFAULT_OPTIONS,
+    ...props,
+  };
+
+  return <CountryPickerComponent {...propsModel} />;
+};
